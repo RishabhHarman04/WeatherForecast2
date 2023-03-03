085611187149a5d20d0c6fcb929c69d5d5b4aa68
RishabhHarman04 <er.23rishabh@gmail.com>
2023-03-03 16:20:00 +0530

    All files

---
+++ WindowsFormsApp5/SettingForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class SettingForm : Form
+    {
+        public WeatherSettings Settings { get; set; }
+        
+       public SettingForm()
+        {
+            InitializeComponent();
+            Settings = new WeatherSettings();
+        }
+        public class WeatherSettings
+        {
+            public List<string> Cities { get; set; }
+            public string RefreshTime { get; set; }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            if (checkedListBoxCities.CheckedItems.Count == 0)
+            {
+                MessageBox.Show(MyStrings.Message);
+                return;
+            }
+           
+           SaveSettings(Settings, checkedListBoxCities, numericUpDownTime);
+           ShowWeatherForm(Settings);
+           //this.Close();
+        }
+        public void SaveSettings(WeatherSettings Settings, CheckedListBox checkedListBoxCities, NumericUpDown numericUpDownTime)
+        {
+            Settings.Cities = new List<string>();
+            foreach (var item in checkedListBoxCities.CheckedItems)
+            {
+                Settings.Cities.Add(item.ToString());
+            }
+            Settings.RefreshTime = numericUpDownTime.Value.ToString();
+        }
+        public void ShowWeatherForm(WeatherSettings Settings)
+        {
+            // Check if an instance of WeatherData already exists
+            WeatherData weatherDataForm = Application.OpenForms.OfType<WeatherData>().FirstOrDefault();
+
+            if (weatherDataForm != null)
+            {
+                this.Hide();
+                weatherDataForm.Closed += (s, args) => this.Close();
+                // Show existing instance of WeatherData form
+                weatherDataForm.Show();
+            }
+            else
+            {
+                this.Hide();
+                // Create new instance of WeatherData form
+                WeatherData weatherData = new WeatherData(Settings);
+                weatherData.Closed += (s, args) => this.Close();
+                weatherData.Show();
+            }
+        }
+        public void InitializeCities(CheckedListBox checkedListBoxCities)
+        {
+            checkedListBoxCities.Items.Add(MyStrings.City1);
+            checkedListBoxCities.Items.Add(MyStrings.City2);
+            checkedListBoxCities.Items.Add(MyStrings.City3);
+            checkedListBoxCities.Items.Add(MyStrings.City4);
+            checkedListBoxCities.Items.Add(MyStrings.City5);
+            checkedListBoxCities.CheckOnClick = true;
+        }
+
+        public void InitializeRefreshTime(NumericUpDown numericUpDownTime)
+        {
+            numericUpDownTime.Minimum = 5;
+            numericUpDownTime.Maximum = 15;
+            numericUpDownTime.Value = 5;
+            numericUpDownTime.Increment = 5;
+        }
+        private void SettingForm_Load_1(object sender, EventArgs e)
+        {
+            InitializeCities(checkedListBoxCities);
+            InitializeRefreshTime(numericUpDownTime);
+        }
+    }
+
+}
+
---
+++ WindowsFormsApp5/WeatherService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static WindowsFormsApp5.WeatherData;
+using static WindowsFormsApp5.SettingForm;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public class WeatherService
+    {
+        private HttpClient httpClient = new HttpClient();
+        private string apiKey = MyStrings.APIKEY;
+        private WeatherSettings settings;
+
+        public  WeatherService(WeatherSettings settings)
+        {
+            this.settings = settings;
+        }
+
+        public async Task<string> GetWeatherData(string city)
+        {
+            try
+            {
+                string url = String.Format(MyStrings.WeatherAPIUrl,city, apiKey);
+                var response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show(String.Format(MyStrings.HttpExceptionMessage,city, ex.Message));
+                return null;
+            }
+        }
+
+        public async Task UpdateLabels(Label label1, Label lblHumidity, Label lblAtmosphere, Label lblTemperature, int panelIndex)
+        {
+            try
+            {
+                var city = settings.Cities.ElementAt(panelIndex);
+                Console.WriteLine(panelIndex.ToString());
+                var weatherData = await GetWeatherData(city); 
+                dynamic json = Newtonsoft.Json.JsonConvert.DeserializeObject(weatherData);
+                double temperature = json.main.temp;
+                double humidity = json.main.humidity;
+                double pressure = json.main.pressure;
+                label1.Text = city;
+                lblHumidity.Text = String.Format(MyStrings.HumidityText, humidity);
+                lblAtmosphere.Text = String.Format(MyStrings.AtmoshphereText, pressure);
+                lblTemperature.Text = String.Format (MyStrings.TemperatureText, temperature);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
